4149e9287525163b2dd2595acaf20786a2bb2d0b
David Durlan <david.a.durlan@gmail.com>
2020-04-12 12:52:34 +0200

    split app into app and fetch

--- src/App.js
+++ src/App.js
@@ -1,87 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import axios from 'axios';
 import './App.css';
-import secrets from './secrets.json';
 import Grid from './grid';
 import Nav from './nav';
+import fetch from './fetch';
 
 function App() {
     const [photos, setPhotos] = useState([]);
     const [page, setPage] = useState(1);
-    const [err, setErr] = useState();
-    const [info, setInfo] = useState();
-    const [showBig, setShowBig] = useState(true);
     const [endpoint, setEndpoint] = useState(`/photos/?`);
+    const [err, setErr] = useState();
+    const [showBig, setShowBig] = useState(true);
 
+    const getPhotos = async (endpoint, page) => {
+        try {
+            let data = await fetch(endpoint, page);
+            setPhotos((state) => [...state, ...data]);
+        } catch (err) {
+            setErr(err);
+        }
+    };
     useEffect(() => {
-        (async () => {
-            try {
-                let { data } = await axios.get(
-                    `https://api.unsplash.com${endpoint}page=${page}&per_page=10&client_id=${secrets.accessKey}`
-                );
-                if (endpoint.includes('search')) {
-                    data = data.results;
-                }
-                if (!data.length) {
-                    setInfo('no results');
-                } else {
-                    setInfo('');
-                    data = addOrientation(data);
-                    data = addPopularity(data);
-                    setPhotos((state) => [...state, ...data]);
-                }
-            } catch (err) {
-                setInfo('');
-                setErr(err);
-            }
-        })();
-    }, [endpoint, page]);
+        getPhotos(`/photos/?`, 1);
+    }, []);
 
     function fetchMore() {
         setPage(page + 1);
+        getPhotos(endpoint, page + 1);
+    }
+
+    function handleEndpoint(val) {
+        // TODO: refactor this
+        setPhotos([]);
+        setPage(1);
+        if (val === '') {
+            getPhotos(`/photos/?`, 1);
+            setEndpoint(`/photos/?`);
+        } else {
+            getPhotos(`/search/photos/?query=${val}&`, 1);
+            setEndpoint(`/search/photos/?query=${val}&`);
+        }
     }
 
     function handleShowBig() {
         setShowBig(!showBig);
     }
 
-    function addOrientation(photos) {
-        photos.map((photo) => {
-            return (photo.orientation =
-                photo.width > photo.height ? 'horizontal' : 'vertical');
-        });
-        return photos;
-    }
-
-    function addPopularity(photos) {
-        const mostLikes = photos.sort((a, b) => {
-            return b.likes - a.likes;
-        })[0].id;
-        photos.map((photo, i) => {
-            return (photo.popular = mostLikes === photo.id);
-        });
-        return photos;
-    }
-
-    function handleEndpoint(val) {
-        setPage(1);
-        setPhotos([]);
-        if (val === '') {
-            setEndpoint(`/photos/?`);
-        } else {
-            setEndpoint(`/search/photos/?query=${val}&`);
-        }
-    }
-
     function renderGrid() {
         if (!photos.length) {
             if (err) {
-                return <p className='error'>{err.message} :(</p>;
-            } else if (info) {
-                return <p className='info'>{info}</p>;
+                return <p className='error'>{err.message}</p>;
             }
-
             return <p className='loading'>Loading...</p>;
         } else {
             return (
@@ -92,7 +61,7 @@ function App() {
                             photos={photos}
                             fetchMore={fetchMore}
                             showBig={showBig}
-                            info={info}
+                            err={err}
                             {...routeProps}
                         />
                     )}
---
+++ src/fetch.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import secrets from './secrets';
+
+const fetch = async (endpoint, page) => {
+    try {
+        let { data } = await axios.get(
+            `https://api.unsplash.com${endpoint}page=${page}&per_page=10&client_id=${secrets.accessKey}`
+        );
+        if (endpoint.includes('search')) {
+            data = data.results;
+        }
+        if (data.length) {
+            data = addOrientation(data);
+            data = addPopularity(data);
+        } else {
+            throw Error('No results');
+        }
+        return data;
+    } catch (err) {
+        throw Error(err);
+    }
+};
+
+function addOrientation(photos) {
+    photos.map(
+        (photo) =>
+            (photo.orientation =
+                photo.width > photo.height ? 'horizontal' : 'vertical')
+    );
+    return photos;
+}
+
+function addPopularity(photos) {
+    const mostLikes = photos.sort((a, b) => b.likes - a.likes)[0].id;
+    photos.map((photo, i) => (photo.popular = mostLikes === photo.id));
+    return photos;
+}
+
+export default fetch;
